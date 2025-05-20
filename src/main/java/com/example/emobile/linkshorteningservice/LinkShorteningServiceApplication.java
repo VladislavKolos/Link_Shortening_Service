8186245dcb04c7c1c39a9610b2ed809ab085c44c
VladislavKolos <vladislav.kolos.y@gmail.com>
2025-05-20 16:50:41 +0300

    Initial commit

---
+++ src/main/java/com/example/emobile/linkshorteningservice/LinkShorteningServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.emobile.linkshorteningservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShorteningServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShorteningServiceApplication.class, args);
+    }
+}
